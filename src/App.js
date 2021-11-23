10383c625b5bdec17f692e8eed92f2a14374f823
MrJacooo <sebastian.storz2003@gmail.com>
2021-11-23 20:27:18 +0100

    Better Drawing Implementation

--- src/App.js
+++ src/App.js
@@ -10,10 +10,10 @@ import './App.css';
 
 //Setup for the Animation Loop
 // loop is a function that is executed every upadteInterval. Modified via changeLoopInterval
-var loop; var updateInterval = setInterval(() =>  loop(), 1);let tempGrid; 
-let keypress = "" //Currently pressed Key
+var loop; var updateInterval = setInterval(() => loop(), 1); let tempGrid;
+let drawType = "" //defines what tile is being drawn at the moment
 
-let startEnd = 1 //Temporary
+let startEnd = 1 //Temporary variable to generate start and end
 
 function App() {
   //Loop Administration
@@ -27,69 +27,70 @@ function App() {
   const [gridY, setGridY] = useState(20)
   const [tileSize, setTilesize] = useState(0)
   const [draw, setDraw] = useState(true)
-  
+
   //This is the Loop thats used to animate the Algorithm. Created via updateInterval
   loop = () => {
-    if(toggleLoop){
-      setCounter(counter+1)
+    if (toggleLoop) {
+      setCounter(counter + 1)
     }
-    keypress = ""
   }
 
   //Used to adjust the Animation Loop.
   //Takes e -> e.target.value and sets that as the new Interval for the Loop
-  function changeLoopInterval(val){
+  function changeLoopInterval(val) {
     console.log(val)
     let tempInterval = 1
-    switch (val){
-      case "1": tempInterval=1;break;
-      case "2": tempInterval=100; break;
-      case "3": tempInterval=500; break;
-      default: tempInterval=1;break;
+    switch (val) {
+      case "1": tempInterval = 1; break;
+      case "2": tempInterval = 100; break;
+      case "3": tempInterval = 500; break;
+      default: tempInterval = 1; break;
     }
-    if(draw) {tempInterval=1} //to set the Interval while drawing
+    if (draw) { tempInterval = 1 } //to set the Interval while drawing
     setLoopInterval(val)
-    clearInterval(updateInterval) 
-    updateInterval = setInterval(() => loop(), tempInterval) 
+    clearInterval(updateInterval)
+    updateInterval = setInterval(() => loop(), tempInterval)
   }
 
   //generates the Grid
-  function generateGrid(){
+  function generateGrid() {
     tempGrid = [];
-    for(let i = 0; i < gridY; i++){
-      tempGrid = [...tempGrid , []] //generate file
-      for(let j = 0; j < gridX; j++){
-        tempGrid[i] = [...tempGrid[i], {x: j, y:i, style: {backgroundColor: "#e7e7e7"}}] //generate ROW, x=ROW y=FILE
+    for (let i = 0; i < gridY; i++) {
+      tempGrid = [...tempGrid, []] //generate file
+      for (let j = 0; j < gridX; j++) {
+        tempGrid[i] = [...tempGrid[i], { x: j, y: i, style: { backgroundColor: "#e7e7e7" } }] //generate ROW, x=ROW y=FILE
       }
     }
     setGrid(tempGrid)
   }
 
   //styles Grid according to Windowsize and Tile Amount
-  function gridStyle(){
-    let xmargin = (window.innerWidth - (window.innerWidth*0.2)) / gridX
-    let ymargin = (window.innerHeight - (window.innerHeight*0.2)) / gridY
-    setTilesize(xmargin<ymargin?xmargin:ymargin) //chooses the smaller size so the Grid fits perfectly
+  function gridStyle() {
+    let xmargin = (window.innerWidth - (window.innerWidth * 0.2)) / gridX
+    let ymargin = (window.innerHeight - (window.innerHeight * 0.2)) / gridY
+    setTilesize(xmargin < ymargin ? xmargin : ymargin) //chooses the smaller size so the Grid fits perfectly
   }
 
-  function drawNode(rowId, tileId){
-    if(draw){
-        //Check which key is Pressed: W=Wand, G=Gewicht
-        if(keypress === "KeyW"){ //Wand
-          tempGrid[rowId][tileId] = {...tempGrid[rowId][tileId], style: {backgroundColor: "#404040"}}
-        }else if(keypress === "KeyG"){ //Gewicht
-          tempGrid[rowId][tileId] = {...tempGrid[rowId][tileId], style: {backgroundColor: "#e7a7a7"}}
-        }
+  function drawNode(rowId, tileId) {
+    if (draw) {
+      //Check which key is Pressed: W=Wand, G=Gewicht
+      if (drawType === "wall") { //Wand
+        tempGrid[rowId][tileId] = { ...tempGrid[rowId][tileId], style: { backgroundColor: "#404040" } }
+      } else if (drawType === "weight") { //Gewicht
+        tempGrid[rowId][tileId] = { ...tempGrid[rowId][tileId], style: { backgroundColor: "#e7a7a7" } }
+      } else if (drawType === "none") { //leer
+        tempGrid[rowId][tileId] = { ...tempGrid[rowId][tileId], style: { backgroundColor: "#e7e7e7" } }
+      }
     }
   }
 
   //Temporary function to draw start and end Nodes
-  function addStartEnd(rowId, tileId){
-    if(startEnd === 1){
-      tempGrid[rowId][tileId] = {...tempGrid[rowId][tileId], style: {backgroundColor: "#109f10", borderRadius: 50}}
+  function addStartEnd(rowId, tileId) {
+    if (startEnd === 1) {
+      tempGrid[rowId][tileId] = { ...tempGrid[rowId][tileId], style: { backgroundColor: "#109f10" } }
     }
-    if(startEnd===2){
-      tempGrid[rowId][tileId] = {...tempGrid[rowId][tileId], style: {backgroundColor: "#10109f"}}
+    if (startEnd === 2) {
+      tempGrid[rowId][tileId] = { ...tempGrid[rowId][tileId], style: { backgroundColor: "#10109f" } }
     }
     startEnd++
   }
@@ -98,21 +99,21 @@ function App() {
   useEffect(() => {
     generateGrid()
     gridStyle()
-  },[])
+  }, [])
 
 
   return (
     <div className="app">
       <div className="header">
         {counter}
-        <button onClick={() => {setToggleLoop(!toggleLoop)}}>Toggle Count Up</button>
+        <button onClick={() => { setToggleLoop(!toggleLoop) }}>Toggle Count Up</button>
         <input type="range" step="1" max="3" min="1" value={loopInterval} onChange={e => changeLoopInterval(e.target.value)}></input>
       </div>
       <div className="grid">
-        {grid.map((row,rowId) => 
-          <div key={"row-"+rowId} style={{height: tileSize}}>
-            {row.map((tile,tileId) => 
-              <button key={"row-"+rowId+"tile-"+tileId} style={{height: tileSize, width: tileSize, ...tile.style}} onMouseOver={() => drawNode(rowId, tileId)} onClick={e => addStartEnd(rowId, tileId)}></button>
+        {grid.map((row, rowId) =>
+          <div key={"row-" + rowId} style={{ height: tileSize }}>
+            {row.map((tile, tileId) =>
+              <button key={"row-" + rowId + "tile-" + tileId} style={{ height: tileSize, width: tileSize, ...tile.style }} onMouseOver={() => drawNode(rowId, tileId)} onClick={e => addStartEnd(rowId, tileId)}></button>
             )}
           </div>
         )}
@@ -121,8 +122,18 @@ function App() {
   );
 }
 
-window.addEventListener("keypress", e => {
-  keypress= e.code
+//Listens for Key Inputs. Press once to Draw, then again to stop Drawing
+window.addEventListener("keydown", e => {
+  switch (e.key) {
+    case "w":
+      drawType = drawType === "" ? "wall" : ""
+      break
+    case "g":
+      drawType = drawType === "" ? "weight" : ""
+      break
+    case " ":
+      drawType = drawType === "" ? "none" : ""
+  }
 })
- 
+
 export default App;
