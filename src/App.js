759cd334468e576c46f31b5177a38353a04c7767
Storz Sebastian <sebastian.storz2003@gmail.com>
2021-11-23 16:39:57 +0100

    Drawing Weight and Walls finished, Speed Adjustment added

--- src/App.js
+++ src/App.js
@@ -6,11 +6,14 @@ import './App.css';
 //TODO: ADD Grid Size customisation
 //TODO: ADD onhover for drawing nodes
 
-
+//TODO: FIX draw speed bug where the speed change does not change actual speed
 
 //Setup for the Animation Loop
 // loop is a function that is executed every upadteInterval. Modified via changeLoopInterval
-var loop; var updateInterval = setInterval(() =>  loop(), 1000)
+var loop; var updateInterval = setInterval(() =>  loop(), 1);let tempGrid; 
+let keypress = "" //Currently pressed Key
+
+
 function App() {
   //Loop Administration
   const [counter, setCounter] = useState(0)
@@ -22,29 +25,40 @@ function App() {
   const [gridX, setGridX] = useState(20)
   const [gridY, setGridY] = useState(20)
   const [tileSize, setTilesize] = useState(0)
+  const [draw, setDraw] = useState(true)
   
   //This is the Loop thats used to animate the Algorithm. Created via updateInterval
   loop = () => {
     if(toggleLoop){
       setCounter(counter+1)
     }
+    keypress = ""
   }
 
   //Used to adjust the Animation Loop.
   //Takes e -> e.target.value and sets that as the new Interval for the Loop
-  function changeLoopInterval(e){
-    setLoopInterval(e.target.value) 
+  function changeLoopInterval(val){
+    console.log(val)
+    let tempInterval = 1
+    switch (val){
+      case "1": tempInterval=1;break;
+      case "2": tempInterval=100; break;
+      case "3": tempInterval=500; break;
+      default: tempInterval=1;break;
+    }
+    if(draw) {tempInterval=1} //to set the Interval while drawing
+    setLoopInterval(val)
     clearInterval(updateInterval) 
-    updateInterval = setInterval(() => loop(), e.target.value) 
+    updateInterval = setInterval(() => loop(), tempInterval) 
   }
 
   //generates the Grid
   function generateGrid(){
-    let tempGrid = [];
+    tempGrid = [];
     for(let i = 0; i < gridY; i++){
       tempGrid = [...tempGrid , []] //generate file
       for(let j = 0; j < gridX; j++){
-        tempGrid[i] = [...tempGrid[i], {x: j, y:i}] //generate ROW, x=ROW y=FILE
+        tempGrid[i] = [...tempGrid[i], {x: j, y:i, style: {backgroundColor: "#e7e7e7"}}] //generate ROW, x=ROW y=FILE
       }
     }
     setGrid(tempGrid)
@@ -57,11 +71,22 @@ function App() {
     setTilesize(xmargin<ymargin?xmargin:ymargin) //chooses the smaller size so the Grid fits perfectly
   }
 
+  function drawNode(rowId, tileId){
+    if(draw){
+        //Check which key is Pressed: W=Wand, G=Gewicht
+        if(keypress === "KeyW"){ //Wand
+          tempGrid[rowId][tileId] = {...tempGrid[rowId][tileId], style: {backgroundColor: "#404040"}}
+        }else if(keypress === "KeyG"){ //Gewicht
+          tempGrid[rowId][tileId] = {...tempGrid[rowId][tileId], style: {backgroundColor: "#e7a7a7"}}
+        }
+    }
+  }
+
   //Setup, only executes at Start
   useEffect(() => {
     generateGrid()
     gridStyle()
-  }, [])
+  },[])
 
 
   return (
@@ -69,13 +94,13 @@ function App() {
       <div className="header">
         {counter}
         <button onClick={() => {setToggleLoop(!toggleLoop)}}>Toggle Count Up</button>
-        <input value={loopInterval} onChange={e => changeLoopInterval(e)}></input>
+        <input type="range" step="1" max="3" min="1" value={loopInterval} onChange={e => changeLoopInterval(e.target.value)}></input>
       </div>
       <div className="grid">
         {grid.map((row,rowId) => 
           <div key={"row-"+rowId} style={{height: tileSize}}>
             {row.map((tile,tileId) => 
-              <button key={"row-"+rowId+"tile-"+tileId} style={{height: tileSize, width: tileSize}}></button>
+              <button key={"row-"+rowId+"tile-"+tileId} style={{height: tileSize, width: tileSize, ...tile.style}} onMouseOver={() => drawNode(rowId, tileId)}></button>
             )}
           </div>
         )}
@@ -83,5 +108,9 @@ function App() {
     </div>
   );
 }
+
+window.addEventListener("keypress", e => {
+  keypress= e.code
+})
  
 export default App;
